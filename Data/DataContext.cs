8a784b6c746dabd407a54c9938202fecb4460aea
CrankyDogs <crankysheep33@gmail.com>
2023-04-26 14:00:18 -0700

    first commit

---
+++ Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using test45.Entitites;
+
+namespace test45.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext( DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
---
+++ Entitites/AppUser.cs
@@ -0,0 +1,9 @@
+﻿namespace test45.Entitites
+{
+    public class AppUser
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
